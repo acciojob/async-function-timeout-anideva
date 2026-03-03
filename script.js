df8ed065a5eb8cc8215d5190c293a3a191b5a229
anideva <anidevarchowdhury@gmial.com>
2026-03-03 13:52:56 +0530

    used async function for dispaying delay message

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+const textInput= document.getElementById("text");
+const delayInput= document.getElementById("delay");
+const button = document.getElementById("btn");
+const outputDiv= document.getElementById("output");
+
+//function to create delay
+function wait(ms) {
+    return new Promise((resolve) =>{
+        setTimeout(resolve,ms);
+    });
+}
+
+//async function
+async function showMessage() {
+    const message= textInput.ariaValueMax;
+    const delay=Number(delayInput.value);
+
+    await wait(delay);
+    outputDiv.textContent=message;
+}
+
+//button click
+button.addEventListener("Click", showMessage);
